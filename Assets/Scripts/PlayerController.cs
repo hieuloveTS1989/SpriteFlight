66038059eea5f60c172099da3dd98bdc965342c5
hieuloveTS1989 <phanconghieutp@gmail.com>
2026-04-14 22:35:53 +0700

    feat: add scoring system
    Users can now see the score on the screen
    
    - Added score UI
    - Calculate and update score real-time

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,9 @@
+using System;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,17 +11,41 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     public float maxSpeed = 5f;
     public GameObject boosterFlame;
+    private float elapsedTime = 0f;
+    private float score = 0f;
+    public float scoreMultiplier = 10f;
+    public UIDocument uIDocument;
+    private Label scoreText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoreText = uIDocument.rootVisualElement.Q<Label>("ScoreLabel");
     }
 
     // Update is called once per frame
     void Update()
+    {   
+        UpdateScore();
+        MovePlayer();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (Mouse.current.leftButton.isPressed)
+        Destroy(gameObject);
+    }
+
+    void UpdateScore()
+    {
+        elapsedTime += Time.deltaTime;
+        score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
+        scoreText.text = "Score: " + score;
+    }
+
+    void MovePlayer()
+    {
+         if (Mouse.current.leftButton.isPressed)
         {
             Vector3 mousePosition = Camera
                 .main.ScreenToWorldPoint(Mouse.current.position.value);
@@ -42,10 +68,5 @@ public class PlayerController : MonoBehaviour
         {
             boosterFlame.SetActive(false);
         }
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Destroy(gameObject);
     }
 }
